19c04c07943775612ae12b6e3e74fca18ead36c5
JHodgkins <jhodgkins78@gmail.com>
2022-06-19 01:22:23 +0000

    update contact text

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ def about_member(member_name):
 def contact():
     if request.method == "POST":
         # print(request.form.get("name"))
-        flash("Thanks {}, We have recieved your message!".format(
+        flash("Thanks! {}, We have recieved your message!".format(
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
